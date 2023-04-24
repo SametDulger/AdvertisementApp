df29b9846d88c79778a13ba15bb9cbcedfbcb8e6
Samet <sametd94@gmail.com>
2023-04-24 20:18:38 +0300

    first commit

---
+++ AdvertisementApp.Entities/MilitaryStatus.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvertisementApp.Entities
+{
+    public class MilitaryStatus:BaseEntity
+    {
+
+        public string Definition { get; set; }
+
+        public List<AdvertisementAppUser> AdvertisementAppUsers { get; set; }
+
+    }
+}
